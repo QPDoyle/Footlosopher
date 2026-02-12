b92b2dd49d7ea98654afca1dec2c035a6a34d876
QPDoyle <qpdoyle13@gmail.com>
2026-02-11 21:11:48 -0500

    joe is back baby!

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import React, {useEffect, useState} from 'react'
+import './App.css';
+
+
+
+const Person = {
+  name: "Joe Devine", 
+  src: "https://pbs.twimg.com/profile_images/1970602489142444032/jBYU5QiW_400x400.jpg",
+  alt: "Joe Devine"
+}
+
+function Avatar(props) {
+  return(<img src={props.src} alt={props.alt}/>)
+}
 
 function App() {
-
   const [backendData, setBackendData] = useState([{}])
   useEffect(() => {
     fetch("/api").then(
@@ -14,6 +26,7 @@ function App() {
   }, [])
   return (
     <div>
+      <Avatar src={Person.src} alt={Person.alt} />
       {(typeof backendData.users === 'undefined') ? (
         <p>Loading...</p>
       ) : (
